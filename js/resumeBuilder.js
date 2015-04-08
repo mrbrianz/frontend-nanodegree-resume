0e234467ed8f0932993c09b39a32aa722d67e078
shesellsseashells@me.com <shesellsseashells@me.com>
2015-04-07 21:06:15 -0500

    Sharpen up the 4 main objects Add pronounce, to display pronunciation of my name on the page correctly Update Skills to include checkmarks, removed 'awesomeness' even though I'm still pretty awesome Pare down the commenting, in case too many comments drive the reader of the code crazy Add better overall content for use in the resume Switch colleges order, so that the last is first as the reader is reading the page Clean up invoking of the display functions Added cornering to many sections of the page (I think it looks nice)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,20 +1,29 @@
+// bio object
 var bio = {
+	// bio info
 	"name" : "Brian Zuehlke",
+	// I added pronounce, because I like for my name to be pronounced correctly :)
+	// I understand the instructions said 'follow the names within the schema below exactly'
+	// but those with hard-to-pronounce names will understand
+	"pronounce" : "zell-key",
 	"role" : "Web Developer, Extraordinaire",
 	"contacts" : {
-		"mobile" : "512-xxx-xxxx",
-		"email" : "mxxxxxx@xx",
+		"mobile" : "512-765-4321",
+		"email" : "mxxxxxx@yyy.com",
 		"github" : "mrbrianz",
-		"blog" : "mrbrianz.com",
+		"twitter" : "@mrbrianz",
+		"blog" : "mrbrianz.com", // blog was available in helper.js, so I included it
 		"location" : "Austin, TX"
 	},
-	"welcomeMessage" : "The one you really want to hire.  Today!",
-	"skills" : ["Awesomeness", "Programming", "Mobile Web", "JavaScript"],
+	"welcomeMessage" : "Building & Developing meaningful web products for use in Business and Customer Services.",
+	"skills" : ["Programming ✔︎", "Coldfusion ✔︎", "Mobile Web ✔︎", "CSS ✔︎", "JavaScript ✔︎"],
 	"bioPic": "images/me.jpg",
+	// bio display function
 	display : function() {
 
-		// set formatted vars using helper.js pre-fabbed elements, but replace %data% with the vars
+		// set formatted vars
 		var formattedName = HTMLheaderName.replace("%data%", bio.name);
+		var formattedPronounce = HTMLheaderNamePronunciation.replace("%data%", bio.pronounce);
 		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 		var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
 		var formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
@@ -24,149 +33,148 @@ var bio = {
 		var formattedblog = HTMLblog.replace("%data%", bio.contacts.blog);
 		var formattedlocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
-		// prepend formatted vars to header id
-		// We back in the header info, because we want it to prepend to the other tags in the id header (before the unordered list 'ul')
+		// prepend formatted vars
 		$("#header").prepend(formattedRole);
+		$("#header").prepend(formattedPronounce);
 		$("#header").prepend(formattedName);
+		// append the rest
 		$("#header").append(formattedBioPic);
 		$("#header").append(formattedwelcomeMsg);
-
-
-		$("#topContacts").append(formattedmobile,formattedemail,formattedgithub,formattedblog,formattedlocation);
+		// contacts append
+		$("#topContacts").append(formattedmobile,formattedemail,formattedgithub,formattedblog/*,formattedlocation *//* location not desired here, just personal preference */);
 		$("#footerContacts").append(formattedmobile,formattedemail,formattedgithub,formattedblog,formattedlocation);
 
-		/* ------------------------------------------
-			Display Skills on page, in unordered list
-			-----------------------------------------  */
-
-		// USING for IN loops (method 3)
-		// example: for(*index* in object/array) {dosomething()};
-
-		// grab header id, append the variable HTMLskillsStart from helper.js
+		// USING for IN loops
+		// append to header
 		$("#header").append(HTMLskillsStart);
 
 		for(nextSkillItem in bio.skills) {
-				// set the var formatted skill to HTMLskills from helper.js, but replace %data% with skills item X (x = nextSkillItem variable)
+				// set formatted var
 				var formattedSkill = HTMLskills.replace("%data%", bio.skills[nextSkillItem]);
-				// grab skills id, append the formattedSkill variable to it
+				// append to skills
 				$("#skills").append(formattedSkill);
 		};
-
 	}
 };
+// bio object end
 
+// education object
 var education = {
 	"schools": [
 		{
-			"name" : "McLennan Community College (yes, just like the tv show 'Community' except funnier)",
-			"location" : "Waco, TX",
-			"degree" : "BBA Transfer",
-			"majors" : "n/a",
-			"dates" : "1995-1997",
-			"url" : "http://www.mclennan.edu"
-		},
-		{
 			"name" : "Baylor University",
 			"location" : "Waco, TX",
-			"degree" : "BBA",
-			"majors" : ["Entrepreneurship","International Business"],
+			"degree" : "Bachelors of Business Administration (BBA)",
+			"majors" : ["Entrepreneurship"," International Business"],
 			"dates" : "1997-1999",
 			"url" : "http://www.baylor.edu"
+		},
+		{
+			"name" : "McLennan Community College",
+			"location" : "Waco, TX",
+			"degree" : "Bachelors of Business Administration (BBA) - Transfer",
+			"majors" : "Transferred to Baylor University",
+			"dates" : "1995-1997",
+			"url" : "http://www.mclennan.edu"
 		}
 	],
 	"onlineCourses" : [
 		{
 			"title" : "Frontend Web Developer Nanodegree",
 			"school" : "Udacity",
-			"dates" : 2015,
+			"date" : 2015,
 			"url" : "http://www.udacity.com"
 		}
 	],
+	// education display function
 	display : function() {
 		for (schoolitem in education.schools) {
+			// append start to education
 			$("#education").append(HTMLschoolStart);
 
+			// set formatted vars
 			var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[schoolitem].name);
 			var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[schoolitem].degree);
-			var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[schoolitem].dates);
+			var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[schoolitem].date);
 			var formattedschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[schoolitem].location);
 			var formattedschoolMajor = HTMLschoolMajor.replace('%data%', education.schools[schoolitem].majors);
 
+			// append vars to last education entry
 			$(".education-entry:last").append(formattedschoolName,formattedschoolDegree,formattedschoolDates,formattedschoolLocation,formattedschoolMajor);
 		};
 
+		// append online courses to last education entry
 		$(".education-entry:last").append(HTMLonlineClasses);
 
 		for (onlineItem in education.onlineCourses) {
+			// set formatted vars
 			var formattedonlineTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[onlineItem].title) + HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineItem].school);
 			var formattedonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[onlineItem].dates);
 			var formattedonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[onlineItem].url);
 
+			// append vars to last education entry
 			$(".education-entry:last").append(formattedonlineTitle,formattedonlineDates,formattedonlineURL);
 		};
 	}
 };
+// education object end
 
+// work object
 var work = {
 	"jobs": [
 	{
-		"employer" : "AT&T",
-		"title" : "Web Developer",
+		"employer" : "AT&T Mobility, LLC",
+		"title" : "Web Developer/Centers Tech Support Manager",
 		"location" : "Austin, TX",
-		"dates" : "2000-Present",
-		"description" : "Build, maintain, and develope internal web systems for use by employees."
+		"dates" : "2009-Present",
+		"description" : "Responsible for design, development, testing and documentation of the coding and jobs necessary to support management initiatives. Understands computer center infrastructure and environments, tools, and processes; assists with production including loads, source, job scheduling data sets and scripts. Works on several or all technical phases of operations. Ability to employ production system utilities and produce software in one or more programming languages. Directs compilation of records and reports concerning production, machine malfunctioning and maintenance. Possesses and applies a broad knowledge of principles, practices, and procedures to the completion of assignments. Works with minimum supervision. May advise on organizational, procedural, and work flow plans, methods, and procedures analysis. Analyzes the results of monitoring the operating systems and recommends changes to improve processing and utilization. "
 
 	},
 	{
-		"employer" : "Self-employed",
-		"title" : "Website Developer",
+		"employer" : "AT&T Inc",
+		"title" : "Workforce Systems Manager",
+		"location" : "Austin, TX",
+		"dates" : "2000-2009",
+		"description" : "Provided management of scheduling systems and progressively grew the systems into a Web Application for the benefit of the Austin Call Center, and later to the larger overall company."
+	},
+	{
+		"employer" : "Self-Employed",
+		"title" : "Web Developer",
 		"location" : "Waco, TX",
 		"dates" : "1999-2000",
-		"description" : "Built websites for various companies who wished to have a web presence."
+		"description" : "Created, developed, and maintained business websites for local businesses. Self-employment involved compliance with all laws, ordinances, and taxes necessary for the operation of a business."
 	},
 	{
 		"employer" : "Madagascar United",
-		"title" : "Wildlife Documentary Filmmaker",
+		"title" : "Wildlife Documentary Filmmaker (Just kidding!)",
 		"location" : "Fianarantsoa, Madagascar",
 		"dates" : "1997-1999",
-		"description" : "Filmed several extraordinary films which were never released.  Also this expands the Google Map so that it looks like I have traveled the world!"
+		"description" : "Directed and Produced several extraordinary films which were never released.  This work experience expands the Google Map so that it looks like I have traveled the world!"
 	}
 	],
-	// create displayWork function containing our 'for in' loop which displays our work section
+	// work display function
 	display : function() {
 		for (jobItemNumber in work.jobs ) {
 
-			// grab workExperience and append HTMLworkstart to it from helper.js
+			// append start to work experience section
 		 	$("#workExperience").append(HTMLworkStart);
 
-		 	// set var formattedEmployer as HTMLworkEmployer from helper.js but replace %data% with work job x, x being the jobItemNumber index being used in this 'for in loop' and use the employee value
+		 	// set formatted vars
 		 	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[jobItemNumber].employer);
-		 	// do same, but for Title
 		 	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[jobItemNumber].title);
-		 	// Concatenate the two vars into one new var
 			var formattedEmployerTitle = formattedEmployer + formattedTitle;
-
 			var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[jobItemNumber].dates);
 			var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[jobItemNumber].location);
 			var formattedDescription = HTMLworkDescription.replace('%data%', work.jobs[jobItemNumber].description);
 
-			// grabbing all class work-entry, grabbing the last one, and appending the concatenated var
-			$(".work-entry:last").append(formattedEmployerTitle);
-			// same for location
-			$(".work-entry:last").append(formattedWorkLocation);
-			// same for dates
-			$(".work-entry:last").append(formattedWorkDates);
-			// same for descriptions
-			$(".work-entry:last").append(formattedDescription);
+			// append vars to last work entry
+			$(".work-entry:last").append(formattedEmployerTitle,formattedWorkLocation,formattedWorkDates,formattedDescription);
 		};
 	}
 };
+// work object end
 
-
-/* 	=============================================
-	Projects Object, containing display function
-	=============================================  */
-
+// projects object
 var projects = {
 	"projects": [
 	{
@@ -182,17 +190,15 @@ var projects = {
 		"images" : ["images/nexus1.jpg","images/nexus2.jpg"]
 	}
 	],
-	// create a function encapsulated in projects.display
+	// projects display function
 	display : function() {
-		// use 'for in' loop for all "projects" in the projects object
-		// loop for all projects.projects, using projectItemNumber as the index
 		for (projectItemNumber in projects.projects) {
-		// append HTMLprojectStart from helper.js to to add project-entry class div
+		// append start to projects section
 		$("#projects").append(HTMLprojectStart);
 
-		// set formatted var by taking HTMLprojectTitle from helper.js and replacing %data% with the projects projects itemNumber title
+		// set formatted vars
 		var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[projectItemNumber].title);
-		// append this formatted var to the project-entry class, last element
+		// append formatted var to last project entry
 		$(".project-entry:last").append(formattedProjectTitle);
 
 		// repeat for dates
@@ -216,13 +222,13 @@ var projects = {
 		};
 	}
 };
+// projects object end
 
 
 /*	===================
 	Capture page clicks
 	===================  */
 
-// grab document, and on click run function loc, which is an event object and has various properties we can set as variables and then pass those to the console
 $(document).click(function(loc) {
 	// set vars from loc properties
 	var x = loc.pageX;
@@ -285,19 +291,25 @@ function inName(theIncomingName) {
 	====================  */
 
 displayBrianResume = function() {
-	// invoke work display
-	work.display();
-	// invoke education display
-	education.display();
-	// invoke projects display
-	projects.display();
 	// invoke bio display
 	bio.display();
+	// invoke work display
+	work.display();
+	// invoke projects display
+	projects.display();
+	// invoke education display
+	education.display();
+
 	// append InternationalizeButton to main
 	$('#main').append(internationalizeButton);
 	// append googleMap to the page
 	$("#mapDiv").append(googleMap);
+
+	// using jquery.corner.js
+	// gives us rounded rectangles
+	$('#map').corner('10px');
+	$('.biopic,.imgProject,#header,#workExperience,#projects,#education,#letsConnect').corner('20px');
 };
 
-// invoke the display of this resume
+// invoke the display of this resume by invoking the displayBrianResume function
 displayBrianResume();
