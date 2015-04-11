29bcaff38d2646caab8bfdf0cbf8a227f79a3575
shesellsseashells@me.com <shesellsseashells@me.com>
2015-04-11 14:51:28 -0500

    Add name to page

--- js/helper.js
+++ js/helper.js
@@ -242,4 +242,4 @@ window.addEventListener('load', initializeMap);
 window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
   map.fitBounds(mapBounds);
-});
+});/* Brian Zuehlke (zell-key), 2015 */
